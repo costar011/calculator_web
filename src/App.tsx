79193624636c5aef76f3ef3a5e943f8f2562d21e
costar011 <dnaer456@gmail.com>
2025-05-18 23:43:13 +0900

    테스트 tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import { ThemeProvider } from '@mui/material/styles';
-import Calculator from './components/Calculator';
-import theme from './theme';
+import { ThemeProvider } from "@mui/material/styles";
+import Calculator from "./components/Calculator";
+import theme from "./theme";
+import Test from "./components/test";
 
 function App() {
   return (
