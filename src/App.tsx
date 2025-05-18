f7b3c72594e9dad7a7216ac51a03d24f1ec655b7
costar011 <dnaer456@gmail.com>
2025-05-18 23:44:01 +0900

    테스트 없앰

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { ThemeProvider } from "@mui/material/styles";
 import Calculator from "./components/Calculator";
 import theme from "./theme";
-import Test from "./components/test";
 
 function App() {
   return (
